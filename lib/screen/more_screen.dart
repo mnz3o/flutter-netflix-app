75873a37b4b8ec74747b9105ee034ea4b643df4f
mnz3o <kimj4056@naver.com>
2026-03-09 15:41:54 +0900

    프로필 및 기타 화면 구성

--- lib/screen/more_screen.dart
+++ lib/screen/more_screen.dart
@@ -3,8 +3,60 @@ import 'package:flutter/material.dart';
 class MoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('더보기', style: TextStyle(color: Colors.white)),
+    return Container(
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 50),
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/netflix_logo.png'),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 15),
+              child: Text(
+                'MINJEONG',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 140,
+              height: 5,
+              color: Colors.red,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                ),
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.edit),
+                      SizedBox(width: 10),
+                      Text('프로필 수정하기'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
